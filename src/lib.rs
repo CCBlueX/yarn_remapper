0ee9c81644b952fb34e7a955d783eb3a5438d604
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-29 23:51:10 +0800

    create method for backwards compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+use std::path::Path;
+use crate::mapping::MappingLoader;
+use crate::tiny_v2::TinyV2Mapping;
+
 pub mod mapping;
 pub mod tiny_v2;
+
+#[deprecated(since = "0.1.0", note = "Please use `TinyV2Mapping::load` instead")]
+pub fn parse_tiny_v2(file_path: &Path) -> anyhow::Result<TinyV2Mapping> {
+    let mapping = TinyV2Mapping::load(file_path)?;
+    Ok(mapping)
+}
